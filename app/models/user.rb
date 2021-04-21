5d6c2b6ec97b60c880c9794667c71430de872875
Shaher-11 <shahershamroukh@gmail.com>
2021-04-21 11:25:18 +0200

    Add vlidation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, uniqueness: true, length: { in: 6..20 }
+
+
 end
